1351a09f94b9f643e43199cb51e5bda9dc9a2d05
Ritik6584 <151844190+Ritik6584@users.noreply.github.com>
2024-04-20 12:44:03 +0530

    Add files via upload

---
+++ Guess the Number/MiniProject1.py
@@ -0,0 +1,21 @@
+# Q. Guess the Number
+
+import random
+
+target = random.randint(1, 100)
+
+while True:
+    userChoice = input("Guess the target or Quit(Q) :")
+    if(userChoice == "Q"):
+        break
+
+    userChoice = int(userChoice)
+    if(userChoice == target):
+        print("Success : Correct Guess !!")
+        break
+    elif(userChoice < target):
+        print("Your number was too small. Take a bigger guess...")
+    else:
+        print("your number was too big. Take a smaler guess...")
+
+print("-----GAME OVER !!-----")
